c3412d65fb8a5576d647fce95450a8d8f9d6c9c3
Nicholas Muturi <nmuturi@uapoldmutual.com>
2020-04-09 09:14:11 +0300

    [fix] fixed sidebar navigation after upgrading template to bootstrap 4

--- res/js/dash.js
+++ res/js/dash.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('#inboundTable').DataTable();
+
+    $(window).bind("load resize", function () {
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+            $('#navToggler').removeClass('hidden');
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+            $('#navToggler').addClass('hidden');
+        }
+    });
 });
--- res/js/template.js
+++ res/js/template.js
@@ -19,13 +19,7 @@
             /*====================================
               LOAD APPROPRIATE MENU BAR
            ======================================*/
-            $(window).bind("load resize", function () {
-                if ($(this).width() < 768) {
-                    $('div.sidebar-collapse').addClass('collapse')
-                } else {
-                    $('div.sidebar-collapse').removeClass('collapse')
-                }
-            });
+            
 
           
      
