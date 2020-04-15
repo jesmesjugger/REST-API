f7e7cd25895030268d6701c429fdd5c59acdb895
Nicholas Muturi <nmuturi@uapoldmutual.com>
2020-04-15 16:27:36 +0300

    [style] refactored the login page and styling

---
+++ demo/old_login.php
@@ -0,0 +1,40 @@
+<?php include('functions.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500&family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="res/css/style.css">
+	<title>Registration system PHP and MySQL</title>
+</head>
+<body>		
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	<form method="post" action="#">
+		
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" required>
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password" required>
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn btn-primary" name="login_btn">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="#">Sign up</a>
+		</p>
+	</form>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</html>
--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <?php include('functions.php') ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registration system PHP and MySQL</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500&family=Roboto&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="res/css/login.css">
+	<title>Registration system PHP and MySQL</title>
 </head>
 <body>
-	<div class="header">
-		<h2>Login</h2>
+	<div class="container-fluid">
+		<div class="logo-overlay">
+			<img src="res/img/om_logo.png" class="brand-logo" alt="logo">
+		</div>
+		<div class="card mx-auto my-5">
+			<h2 class="card-header text-center">OM Portal</h2>
+			<div class="card-body">
+				<form action="index.php" method="post">
+					<div class="form-group">
+						<label for="username">Username</label>
+						<input type="text" name="username" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" name="password" class="form-control" required>
+					</div>
+
+					<button type="submit" class="btn btn-outline-success form_btn" name="login_btn">Login</button>
+				</form>
+				<?php echo display_error(); ?>
+			</div>
+		</div>
 	</div>
-	<form method="post" action="index.php">
-		<?php echo display_error(); ?>
-		<div class="input-group">
-			<label>Username</label>
-			<input type="text" name="username" required>
-		</div>
-		<div class="input-group">
-			<label>Password</label>
-			<input type="password" name="password" required>
-		</div>
-		<div class="input-group">
-			<button type="submit" class="btn btn-primary" name="login_btn">Login</button>
-		</div>
-		<p>
-			Not yet a member? <a href="register.php">Sign up</a>
-		</p>
-	</form>
 </body>
 
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
-  <script src=""></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
 </html>
